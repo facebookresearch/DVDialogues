153129514db2db3881e54edd7c03a9d127c03e49
Satwik Kottur <satwik27@gmail.com>
2021-06-02 10:43:21 -0700

    License Headers

--- dvd_generation/question_templates/create_templates.sh
+++ dvd_generation/question_templates/create_templates.sh
@@ -1,3 +1,10 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+All rights reserved.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
 python add_action_attribute.py
 python add_atomic_action_query.py
 python add_compositional_action_query.py
